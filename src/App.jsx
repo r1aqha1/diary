fb0ef7c1355b2b76fc92e723542fcf14752c532e
kimbomi <qhalqhal1231@naver.com>
2024-08-23 17:22:21 +0900

    Published

--- src/App.jsx
+++ src/App.jsx
@@ -133,7 +133,7 @@ function App() {
           <Routes>
             <Route path="https://gi1dong.github.io/diary_test" element={<Home />} />
             <Route path="https://gi1dong.github.io/diary_test/new" element={<New />} />
-            <Route path="/https://gi1dong.github.io/diary_test/diary/:id" element={<Diary />} />
+            <Route path="https://gi1dong.github.io/diary_test/diary/:id" element={<Diary />} />
             <Route path="https://gi1dong.github.io/diary_test/edit/:id" element={<Edit />} />
             <Route path="*" element={<Notfound />} />
           </Routes>

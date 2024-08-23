cde8428459242ce23988381086caeb1b5482375a
kimbomi <qhalqhal1231@naver.com>
2024-08-23 16:44:56 +0900

    route path 수정

--- src/App.jsx
+++ src/App.jsx
@@ -131,10 +131,10 @@ function App() {
           }}
         >
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/new" element={<New />} />
-            <Route path="/diary/:id" element={<Diary />} />
-            <Route path="/edit/:id" element={<Edit />} />
+            <Route path="https://gi1dong.github.io/diary_test/" element={<Home />} />
+            <Route path="https://gi1dong.github.io/diary_test/new" element={<New />} />
+            <Route path="https://gi1dong.github.io/diary_test/diary/:id" element={<Diary />} />
+            <Route path="https://gi1dong.github.io/diary_test/edit/:id" element={<Edit />} />
             <Route path="*" element={<Notfound />} />
           </Routes>
         </DiaryDispatchContext.Provider>
